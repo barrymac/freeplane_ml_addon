78c0010bc8d828c5880faf9b538c3ec5c50dd02a
barry <git@coining.it>
2025-04-10 22:24:34 -0300

    style: Adjust image prompt editor dialog size for better usability

--- src/main/groovy/com/barrymac/freeplane/addons/llm/ui/ImagePromptEditor.groovy
+++ src/main/groovy/com/barrymac/freeplane/addons/llm/ui/ImagePromptEditor.groovy
@@ -291,7 +291,7 @@ class ImagePromptEditor {
                 }
 
                 dialog.rootPane.defaultButton = generateButton
-                dialog.preferredSize = new Dimension(1000, 900) // Increased height
+                dialog.preferredSize = new Dimension(1200, 800) // Increased height
                 dialog.pack()
                 dialog.setLocationRelativeTo(ui.currentFrame)
                 dialog.visible = true // Show the modal dialog
